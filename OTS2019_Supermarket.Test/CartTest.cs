7812d7f9243db7017eac7d63d9e12f09e9357be5
NemanjaJosic <nemanjajosic25@gmail.com>
2026-04-15 14:40:34 +0200

    Vezbe 5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -1,15 +1,30 @@
 ﻿using NUnit.Framework;
 using OTS_Supermarket.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCart_ShouldAddOneToCart_Success()
+        {
+            // ARRANGE
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+            
+            // ACT
+            cart.AddOneToCart(monitor);
+
+            // ASSERT
+            Assert.That(cart.Size, Is.EqualTo(2));
+            Assert.That(cart.Amount, Is.EqualTo(100));
+
+        }
+
+
+
 
     }
 }
